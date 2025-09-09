97ca7281b6858d698b2b702dac946d935d596215
Shuai Wang <shuwang@f4r11s16.clusters.42paris.fr>
2025-09-09 21:13:27 +0200

    try to understand back probagation

--- srcs/forward.py
+++ srcs/forward.py
@@ -11,10 +11,6 @@ def sigmoid(value: array):
 	return 1.0 / (1.0 + math.e ** -value)
 
 
-def weighted_sum(li1, li2, bias):
-	return sum([li1[i] * li2[i] for i in range(len(li1))]) + bias
-
-
 def forward(arr1:array, arr2:array, bias:float):
 
 	print("[ARR1]\n", arr1)
@@ -23,6 +19,18 @@ def forward(arr1:array, arr2:array, bias:float):
 	res = arr1 @ arr2 + bias
 	activ = sigmoid(res)
 	return activ
+
+
+def back_probagation(nets:array, truth:array, res:list):
+	length = len(nets)
+
+	for i in range(length):
+		# result = res[length - i - 1]
+		# print(result)
+		# diff = result - truth #[2d vector]
+		print(f"\033[32mWEIGHT layer[{length - i - 1}]\n", nets[length - i - 1])
+		# print("[DIFF]", diff)
+	print("\033[0m", end="")
 
 
 def network(net: tuple):
@@ -38,11 +46,17 @@ def network(net: tuple):
 
 def main():
 	input = np.array([0.4, 0.3, 0.8])
+	truth = np.array([0.28, 0.72])
 	activ = input
 	nets = network((3, 5, 5, 5, 2))
-	for i in range(len(nets) - 1):
+	actives = [activ]
+	for i in range(len(nets)):
 		activ = forward(activ, nets[i], 0.5)
+		actives.append(activ)
 		print("\033[33m[RES]", activ, "\033[0m")
+
+	print(actives)
+	back_probagation(nets, truth, actives)
 
 
 if __name__ == "__main__":
